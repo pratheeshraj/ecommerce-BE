0bd31a1530379627630455c99d385e25fb9c7cf9
pratheeshraj <rajeshnsk5@gmail.com>
2023-02-27 21:39:22 +0530

    a

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const dotenv=require("dotenv").config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({
-    origin: `${process.env.CORS_URL}`,
-    credentials:true,
-    optionSuccessStatus: 200,
-}));
+app.use(cors())
+
 
 app.use(cookieParser());
 app.use(fileupload()); 
