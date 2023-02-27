721125e33892ac7d8e484a1a4cb4273c4f3b17ba
pratheeshraj <rajeshnsk5@gmail.com>
2023-02-27 21:52:23 +0530

    d

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
 const connectDB = async ()=>{
-   await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false}).then(() => console.log("Database connected successfully.")).catch((err) => {console.log(err);
+   
+   await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true,}).then(() => console.log("Database connected successfully.")).catch((err) => {console.log(err);
     })
 }
 module.exports = connectDB;
